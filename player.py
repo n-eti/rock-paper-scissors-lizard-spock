d56d5fab81dd807c0e6a150fa56728c7d960cf73
Nada Atieh <nada.s.atieh@gmail.com>
2022-12-21 15:52:42 -0600

    We added bools to the players class.

--- player.py
+++ player.py
@@ -1,14 +1,34 @@
 class Player:
-    def __init__(self, player_1, player_2, player_3):
+    def __init__(self, player_1, player_2, player_3, rock, paper, scissors, lizard, spock):
         self.player_1 = player_1
         self.player_2 = player_2
         self.player_3 = player_3
+        self.rock = rock
+        self.scissors = scissors
+        self.paper = paper
+        self.lizard = lizard
+        self.spock = spock
+
 
     def how_many_players(self):
         input("How many players will play in this game? 1, 2, or 3 ")
     
     def player_gestures(self):
-        response = input("Rock, Paper, Scissors, Lizard, Spock? ")
+        # response = input("Rock, Paper, Scissors, Lizard, Spock? "):
+        self.paper > self.rock
+        self.scissors > self.paper
+        self.rock > self.lizard
+        self.lizard > self.spock
+        self.spock > self.scissors
+        self.scissors > self.lizard
+        self.lizard > self.paper
+        self.paper > self.spock
+        self.spock > self.rock
+        self.rock > self.scissors
+        
+
+
+
 
     def winning_choice(self):
         pass

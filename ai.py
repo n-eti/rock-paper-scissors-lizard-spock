a2ff8c48a5a69f1a39649171da7c2c02983817ea
Aubrey Guishard <aubrey.guishard@gmail.com>
2022-12-27 13:09:40 -0500

    Attempted to troubleshoot the choice function for the ai Class

--- ai.py
+++ ai.py
@@ -4,6 +4,6 @@ class Ai(Player):
     def __init__(self):
         super().__init__()
 
-    def choose_gesture(self):
-        self.chosen_gesture = random.randrange(len(Player.gestures))
-        print(self.chosen_gesture)
+    def choose_gesture_ai(self):
+        self.ai_gesture = random.choice(Player.__init__)
+        print(self.ai_gesture)
